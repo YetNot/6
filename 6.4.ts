92cb117e23c5586381df944df0ea48096285e8cd
Glinskikh Kirill <108388350+YetNot@users.noreply.github.com>
2024-01-08 21:36:26 +0300

    Create 6.4.ts

---
+++ 6.4.ts
@@ -0,0 +1,36 @@
+type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
+}; 
+
+type X = { 
+    x: { 
+      a: 1
+      b: 'hi'
+      z: string
+    }
+    y: string
+}
+  
+type Expected = { 
+    readonly x: { 
+        readonly a: 1
+        readonly b: 'hi'
+    }
+    readonly y: 'hey' 
+}
+  
+type Todo = DeepReadonly<X> // should be same as `Expected`
+
+
+const test: Todo = {
+    x: {
+        a: 1,
+        b: 'hi',
+        z: 'try change me too',
+    },
+    y: 'try change me',
+};
+
+
+test.y = 'changed'; // Error
+test.x.z = 'changed'; // Error
